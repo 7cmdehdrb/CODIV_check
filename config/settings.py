9e0f4bfe7dea4c665b27485344d202b20fe68193
7cmdehdrb <7cmdehdrb@naver.com>
2020-05-20 15:32:44 +0900

    change env

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "6mbnl&enp_9ejrjo^js+te7t&gez9gq*1h=u#u1@l@($!jw9!*"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
